5ca056098b8c15531eed0ffc0d9fa20e3c49bb43
Hafiz Raheel Siddique <siddiqueraheel216@gmail.com>
2022-03-09 18:08:04 +0500

    done all skills pge start contact page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
+import Contact from './components/Contact/Contact';
+
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
        <Home />
        <About />
        <Skills />
+       <Contact />
+      
     </div>
   );
 }
